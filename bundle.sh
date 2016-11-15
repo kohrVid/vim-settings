fa8ce4bde79b1e481526036468ee9ed493701221
Jess <kohrVid@zoho.com>
2016-11-15 05:48:12 +0000

    backup vim bundle file

---
+++ bundle.sh
@@ -0,0 +1,15 @@
+(cd ~/.vim/bundle;
+git clone https://github.com/Valloric/YouCompleteMe.git;
+git clone https://github.com/mattn/emmet-vim.git;
+git clone https://github.com/claco/jasmine.vim.git;
+git clone https://github.com/scrooloose/nerdtree.git;
+git clone https://github.com/jwalton512/vim-blade.git;
+git clone https://github.com/kchmck/vim-coffee-script.git;
+git clone https://github.com/xolox/vim-colorscheme-switcher.git;
+git clone https://github.com/fatih/vim-go.git;
+git clone https://github.com/briancollins/vim-jst.git;
+git clone https://github.com/xolox/vim-misc.git;
+git clone git://github.com/uarun/vim-protobuf.git;
+git clone https://github.com/vim-ruby/vim-ruby.git;
+git clone https://github.com/slim-template/vim-slim.git;
+git clone https://github.com/mzlogin/vim-markdown-toc.git;)
