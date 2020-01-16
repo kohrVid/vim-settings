ee5267278f500971d2c6b775ebd9d1b2c2815138
kohrVid <kohrVid@zoho.com>
2020-01-16 18:54:46 +0000

    Move PlugInstall command
    The PlugInstall command may as well run even if the vim config files
    have been set up.

--- bundle.sh
+++ bundle.sh
@@ -9,11 +9,11 @@ then
   cp ./config/coc-settings.json ~/.vim/coc-settings.json
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  vim +PlugInstall +PlugClean! +qall
 else
   echo "Pathogen file present. Please manually add vimrc and colours scheme where needed."
 fi
 
+vim +PlugInstall +PlugClean! +qall
 (vim -u NONE -c "helptags vim-rhubarb/doc" -c q;)
 
 if [ ! -d ~/.vim/ftdetect ]
