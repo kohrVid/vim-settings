06a3d5eefef5134a01429ddd0a02122f2300bf84
Jessica Été <kohrVid@zoho.com>
2020-02-04 13:55:23 +0000

    Make it so you only input the sudo password once
    Have added here strings, &c. so that a sudo password will only need to
    be input once when the arch script is run. May add something similar for
    the Debian version when I have time.
    Have also removed the snap installation as it was only used for Spotify
    and it hasn't worked on two out of three of the machines I tried to
    install it with. There's an AUR package that does the job better.

--- new_arch_machine_setup.sh
+++ new_arch_machine_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 main() {
+  echo "[sudo] password for $USER: "
+  read -s PASSWORD
   echo "Please enter your full name..."
   read GIT_NAME
   echo "Please enter your Github email address..."
@@ -21,26 +23,26 @@ main() {
 
   mkdir -p $HOME/Documents/go $HOME/Documents/Programmes $HOME/Documents/vim
 
-  sudo pacman -Syyu --noconfirm
-  sudo pacman -S --noconfirm clamav make yay base-devel xclip
+  sudo -S <<< "$PASSWORD" pacman -Syyu --noconfirm
+  sudo -S <<< "$PASSWORD" pacman -S --noconfirm clamav make yay base-devel xclip
 
   git clone https://github.com/helixarch/debtap.git
-  sudo ln -s $HOME/Documents/Programmes/debtap/debtap /usr/bin/debtap
+  sudo -S <<< "$PASSWORD" ln -s $HOME/Documents/Programmes/debtap/debtap /usr/bin/debtap
 
-  clamScan "$RUN_SCAN"
-  gitConfig "$GIT_NAME" "$GIT_EMAIL"
-  goInstall "1.12.1"
-  vimConfig
-  tmuxInstall "$IS_A_VM"
-  terraformInstall
+  clamScan "$RUN_SCAN" "$PASSWORD"
+  gitConfig "$GIT_NAME" "$GIT_EMAIL" "$PASSWORD"
+  goInstall "1.12.1" "$PASSWORD"
+  vimConfig "$PASSWORD"
+  tmuxInstall "$IS_A_VM" "$PASSWORD"
+  terraformInstall "$PASSWORD"
   rubyInstall "$INSTALL_RUBY"
-  scalaInstall "$INSTALL_SCALA"
-  dockerInstall "$INSTALL_DOCKER"
-  haskellInstall "$INSTALL_HASKELL"
+  scalaInstall "$INSTALL_SCALA" "$PASSWORD"
+  dockerInstall "$INSTALL_DOCKER" "$PASSWORD"
+  haskellInstall "$INSTALL_HASKELL" "$PASSWORD"
 
   if [ `echo $IS_A_VM | awk '{print toupper($0)}'` = "N" ]
   then
-    postGNOMEInstall
+    postGNOMEInstall "$PASSWORD"
   fi
 
   echo "Setup complete"
@@ -52,9 +54,9 @@ clamScan() {
     echo "Skipping scan"
   else
     echo "Running scan...."
-    sudo mkdir /clam01
-    sudo freshclam
-    sudo clamscan --recursive=yes / --move=/clam01 -l /clam01.txt
+    sudo -S <<< "$2" mkdir /clam01
+    sudo -S <<< "$2" freshclam
+    sudo -S <<< "$2" clamscan --recursive=yes / --move=/clam01 -l /clam01.txt
   fi
 }
 
@@ -65,10 +67,10 @@ gitConfig() {
   else
     git config --global --replace-all user.name "$1"
     git config --global --replace-all user.email "$2"
-    sudo git config --system core.editor vim
+    sudo -S <<< "$3" git config --system core.editor vim
     mv config/gitignore_global $HOME/.gitignore_global
     git config --global core.excludesfile $HOME/.gitignore_global
-    sudo apt-get install kdiff3
+    sudo -S <<< "$3" apt-get install kdiff3
     git config --global --add merge.tool kdiff3
   fi
 }
@@ -76,7 +78,7 @@ gitConfig() {
 goInstall() {
   cd $HOME/Documents/Programmes/
   curl -O "https://dl.google.com/go/go$1.linux-amd64.tar.gz"
-  sudo tar -C /usr/local -xzf "go$1.linux-amd64.tar.gz"
+  sudo -S <<< "$2" tar -C /usr/local -xzf "go$1.linux-amd64.tar.gz"
   echo "export PATH=\$PATH:/usr/local/go/bin" >> $HOME/.bashrc
   echo "export GOPATH=$HOME/Documents/go" >> $HOME/.bashrc
   source $HOME/.bashrc
@@ -85,7 +87,7 @@ goInstall() {
 vimConfig() {
   nodeInstall
   cd $HOME/Documents/vim
-  sudo pacman -S --noconfirm gvim
+  sudo -S <<< "$1" pacman -S --noconfirm gvim
 
 
   (git clone https://github.com/kohrVid/vim-settings.git;
@@ -98,19 +100,19 @@ vimConfig() {
 tmuxInstall() {
   if [[ -z $(which yacc) ]]
     then
-      sudo pacman -S --noconfirm bison
+      sudo -S <<< "$2" pacman -S --noconfirm bison
       alias yacc="bison"
   fi
 
   cd $HOME/Documents/Programmes/
-  sudo pacman -S --noconfirm gcc pkg-config autogen automake
+  sudo -S <<< "$2" pacman -S --noconfirm gcc pkg-config autogen automake
   git clone https://github.com/tmux/tmux.git
   cd tmux
   git remote remove origin
   git remote add origin git@github.com:tmux/tmux.git
   sh autogen.sh
   ./configure && make
-  sudo ln -s $HOME/Documents/Programmes/tmux/tmux /usr/bin/tmux
+  sudo -S <<< "$2" ln -s $HOME/Documents/Programmes/tmux/tmux /usr/bin/tmux
 
   if [ `echo "$1" | awk '{print toupper($0)}'` = "Y" ]
   then
@@ -142,19 +144,19 @@ scalaInstall() {
   then
     cd $HOME/Documents/Programmes/
     echo "Installing scala...."
-    sudo pacman -S --noconfirm default-jre default-jdk
+    sudo -S <<< "$2" pacman -S --noconfirm default-jre default-jdk
     curl -O https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz
-    sudo tar xvf openjdk-11.0.1_linux-x64_bin.tar.gz --directory /usr/lib/jvm/
+    sudo -S <<< "$2" tar xvf openjdk-11.0.1_linux-x64_bin.tar.gz --directory /usr/lib/jvm/
     /usr/lib/jvm/jdk-11.0.1/bin/java -version
     java_home="export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64\nexport PATH=\$JAVA_HOME/bin/:\$PATH"
     echo $java_home >> $HOME/.bashrc
-    sudo echo $java_home >> /etc/environment
-    sudo pacman -S --noconfirm scala
+    sudo -S <<< "$2" echo $java_home >> /etc/environment
+    sudo -S <<< "$2" pacman -S --noconfirm scala
     echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-    sudo pacman -Syyu --noconfirm
-    sudo pacman -S --noconfirm  sbt
-    vimMetals
+    sudo -S <<< "$2" apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+    sudo -S <<< "$2" pacman -Syyu --noconfirm
+    sudo -S <<< "$2" pacman -S --noconfirm  sbt
+    vimMetals "$2"
   else
     echo "Skipping scala installation"
   fi
@@ -162,10 +164,10 @@ scalaInstall() {
 
 vimMetals() {
   cd /usr/bin
-  sudo curl -L -o coursier https://git.io/coursier
-  sudo chmod +x coursier
+  sudo -S <<< "$1" curl -L -o coursier https://git.io/coursier
+  sudo -S <<< "$1" chmod +x coursier
 
-  sudo coursier bootstrap \
+  sudo -S <<< "$1" coursier bootstrap \
     --java-opt -Xss4m \
     --java-opt -Xms100m \
     --java-opt -Dmetals.client=coc.nvim \
@@ -189,16 +191,16 @@ terraformInstall() {
   cd $HOME/Documents/Programmes
   curl -O https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
   unzip terraform_0.11.13_linux_amd64.zip
-  sudo ln -s $HOME/Documents/Programmes/terraform /usr/bin/terraform
+  sudo -S <<< "$1" ln -s $HOME/Documents/Programmes/terraform /usr/bin/terraform
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/wata727/tflint/v0.7.5/install_linux.sh)"
 }
 
 dockerInstall() {
   if [ `echo "$1" | awk '{print toupper($0)}'` = "Y" ]
   then
-    sudo pacman -Syyu --noconfirm
-    sudo pacman -S --noconfirm docker
-    sudo systemctl start docker # Note, any VPNs must be switched off first
+    sudo -S <<< "$2" pacman -Syyu --noconfirm
+    sudo -S <<< "$2" pacman -S --noconfirm docker
+    sudo -S <<< "$2" systemctl start docker # Note, any VPNs must be switched off first
   else
     echo "Skipping docker installation"
   fi
@@ -207,7 +209,7 @@ dockerInstall() {
 haskellInstall() {
   if [ `echo "$1" | awk '{print toupper($0)}'` = "Y" ]
   then
-    sudo pacman -S --noconfirm ghc
+    sudo -S <<< "$2" pacman -S --noconfirm ghc
     cd Documents/Programmes
     curl -sSL https://get.haskellstack.org/ | sh
     git clone https://github.com/haskell/haskell-ide-engine --recurse-submodules
@@ -215,7 +217,7 @@ haskellInstall() {
     stack ./install.hs hie-8.4.4
     stack ./install.hs build-doc
     stack ./install.hs cabal-ghcs
-    sudo ln -s $HOME/.local/bin/hie-wrapper /usr/bin/hie-wrapper
+    sudo -S <<< "$2" ln -s $HOME/.local/bin/hie-wrapper /usr/bin/hie-wrapper
   else
     echo "Skipping haskell installation"
   fi
@@ -223,20 +225,20 @@ haskellInstall() {
 
 postGNOMEInstall() {
   cd $HOME/Documents/Programmes
-  sudo pacman -S --noconfirm conky
+  sudo -S <<< "$1" pacman -S --noconfirm conky
   cp $HOME/Documents/vim/vim-settings/config/conkyrc $HOME/.conkyrc
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   cp $HOME/Documents/vim/vim-settings/config/zshrc $HOME/.zshrc
   cp $HOME/Documents/vim/vim-settings/config/zshenv_arch $HOME/.zshenv
-  guiAppInstall
+  guiAppInstall $1
 }
 
 guiAppInstall() {
   cd $HOME/Documents/Programmes
   echo 1 | yay --noconfirm --answerdiff=None cherrytree
-  anacondaInstall
-  sudo pacman -S --noconfirm community/slack-online
-  spotifyInstall
+  anacondaInstall "$1"
+  sudo -S <<< "$1" pacman -S --noconfirm community/slack-online
+  spotifyInstall "$1"
   pacman -S python2
   #echo 1 | yay --noconfirm --answerdiff=None gnome-python-desktop
 }
@@ -244,35 +246,19 @@ guiAppInstall() {
 anacondaInstall() {
   cd $HOME/Documents/Programmes
   curl -O https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86_64.sh
-  sudo chmod +x Anaconda3-2018.12-Linux-x86_64.sh
+  sudo -S <<< "$1" chmod +x Anaconda3-2018.12-Linux-x86_64.sh
   ./Anaconda3-2018.12-Linux-x86_64.sh
-  conda install -c anaconda-cluster scala
-  conda install -c r r-irkernel rpy2
-  conda install jupyter
+  source $HOME/.zshrc
+  echo "y" | conda install -c anaconda-cluster scala
+  echo "y" | conda install -c r r-irkernel rpy2
+  echo "y" | conda install jupyter
 }
 
 spotifyInstall() {
   cd $HOME/Documents/Programmes
-  git clone https://aur.archlinux.org/snapd.git
-  cd snapd
-  makepkg -s --noconfirm
-  sudo snap install spotify
-}
-
-whatIsMyDistro() {
-  if [[ "`hostnamectl`" =~ 'Ubuntu' ]]
-  then
-    echo "ubuntu"
-  elif [[ "`hostnamectl`" =~ 'Debian' ]]
-  then
-    echo "debian"
-  #elif [[ "`hostnamectl`" =~ 'Fedora' ]]
-  #then
-    #echo "fedora"
-  else
-    echo "Unknown distro" 1>&2
-    exit 1
-  fi
+  git clone https://aur.archlinux.org/spotify.git
+  cd spotify
+  echo "$1" | makepkg -s --noconfirm
 }
 
 main
